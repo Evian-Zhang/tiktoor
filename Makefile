20d80cecebcb58bc253f11d5591667673b733837
Evian-Zhang <evianzhang1999@163.com>
2021-10-27 09:51:06 +0800

    Remove invisible char

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ $(BUILD_DIR):
 	mkdir -p $(BUILD_DIR)/port_hiding
 	mkdir -p $(BUILD_DIR)/process_hiding
 	mkdir -p $(BUILD_DIR)/process_protection
+	mkdir -p $(BUILD_DIR)/backdoor_for_root
 lkm: $(BUILD_DIR)
 	make -C /lib/modules/$(shell uname -r)/build M=$(BUILD_DIR) src=$(PWD)/lkm
 clean:
--- lkm/protocol.h
+++ lkm/protocol.h
@@ -1,27 +1,23 @@
 #define TIKTOOR_IOCTL_CMD 0xdeadbeaf
 
 struct TiktoorCmdArg {
-    unsigned char action;
-    void *subargs;
+    unsigned char action;
+    void *subargs;
 };
 
 struct ProcessHidingSubargs {
-    unsigned int pid;
+    unsigned int pid;
 };
 
 struct DriverHidingSubargs {
-<<<<<<< HEAD
     const char* name;
-=======
-    const char* name;
->>>>>>> 57507f202da51015bbd238624e94aa6a9968057f
 };
 
 struct PortHidingSubargs {
-    unsigned char transmission_type;
-    unsigned short port;
+    unsigned char transmission_type;
+    unsigned short port;
 };
 
 struct ProcessProtectingSubargs {
-    unsigned int pid;
+    unsigned int pid;
 };
